d79180d67fcb1c419d9fd839d6034c3026ab5634
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-02-20 17:16:08 -0500

    Added FeedbackStats, Adjusted to have prop drilling for functioning component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Feedbackitem from "./Feedbackitem";
+import PropTypes from "prop-types";
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0) {
         return <p>No Feed Back Yet</p>
     }
@@ -10,10 +11,16 @@ function FeedbackList({feedback}) {
     return (
         <div className='feedback-list'>
     {feedback.map((item) => (
-       <Feedbackitem key = {item.id} item = {item}/>
+       <Feedbackitem key = {item.id} item = {item}
+       handleDelete = {handleDelete}/>
     ))}
         </div>
     )
 }
 
+FeedbackList.propTypes = {
+    feedback: PropTypes.array
+}
+
 export default FeedbackList;
+
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import Card from "../shared/Card";
+import PropTypes from "prop-types";
 import {useState} from "react";
+import {FaTimes} from 'react-icons/fa';
 
 
-const MyComponent = ({item}) => {
+
+const MyComponent = ({item, handleDelete}) => {
+
 
     return (
-        <div className = 'card'>
+        <Card>
             <div className='num-display'> {item.rating} </div>
-            <div className="text-display">
-                {item.text}
-            </div>
-        </div>
+            <button onClick= {() => handleDelete(item.id)} className="close"><FaTimes color = 'purple'/></button>
+            <div className="text-display">{item.text} </div>
+        </Card>
     );
 };
 
+
+MyComponent.propTypes = {
+    item: PropTypes.object.isRequired
+}
 export default MyComponent;
+
