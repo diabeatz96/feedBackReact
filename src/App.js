e3e8aa9e229e0e4e392314ce91481a4dc26f9401
bigbl <adamkostandy@gmail.com>
2022-02-27 11:51:47 -0500

    FA Question Mark for processing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {useState} from "react";
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 import {v4 as uuidv4} from 'uuid';
 
 function App() {
@@ -25,12 +28,29 @@ function App() {
 
     return (
         <>
+        <Router>
         <Header />
             <div className="container">
-                <FeedbackForm handleAdd = {addFeedback}/>
-                <FeedbackStats feedback = {feedback}/>
-              <FeedbackList feedback = {feedback} handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route path='/*' element = {
+                        <>
+                            <FeedbackForm handleAdd = {addFeedback}/>
+                            <FeedbackStats feedback = {feedback}/>
+                            <FeedbackList feedback = {feedback} handleDelete={deleteFeedback}/>
+                        </>
+                    }>
+                    </Route>
+
+                    <Route path='/about' element = {<AboutPage/>} />
+
+
+                </Routes>
+
+
             </div>
+            <AboutIconLink/>
+
+        </Router>
 
         </>
 
