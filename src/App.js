4d8dd3e990a1e8d21e2b4780cfc1971b2ad8a208
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-03-16 16:48:35 -0400

    Build Changes 2

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                     <Route path='/*' element = {
                         <>
                             <FeedbackForm />
-                            <FeedbackStats feedback={}/>
+                            <FeedbackStats/>
                             <FeedbackList />
                         </>
                     }>
